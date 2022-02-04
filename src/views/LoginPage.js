b559d303a95e242cd91f1af9de1950d47ac80e87
Chakkapong <chakkapong1999@gmail.com>
2022-02-04 11:22:19 +0700

    commit

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -56,7 +56,7 @@ export default function LoginPage() {
           alert("NOT SUCCESS");
         }
       });
-  }, [passwordLogin, usernameLogin]);
+  }, [dispatch, navigate, passwordLogin, usernameLogin]);
 
   const handleCreateUser = useCallback(async () => {
     api
