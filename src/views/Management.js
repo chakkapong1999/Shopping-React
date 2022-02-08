63cda9464c90bcb941969b607c913e238b5eed54
Chakkapong <chakkapong1999@gmail.com>
2022-02-08 13:34:44 +0700

    commit

--- src/views/Management.js
+++ src/views/Management.js
@@ -3,6 +3,8 @@ import Navigation from "../components/Navigation";
 import { api } from "../services/index";
 import { Container, Table, ButtonGroup, Button } from "react-bootstrap";
 import { BsTrash, BsPencilFill, BsPlus } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
 import ModalAddProduct from "../components/ModalAddProduct";
 import ModalEditProduct from "../components/ModalEditProduct";
 import ModalAddInventory from "../components/ModalAddInventory";
@@ -11,6 +13,9 @@ export default function Management() {
   const [products, setProducts] = useState([]);
   const [editProduct, setEditProduct] = useState({});
   const [inStock, setInStock] = useState({});
+
+  const user = useSelector((state) => state.user);
+  const username = user.username;
 
   const [showAddModal, setShowAddModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
@@ -43,83 +48,87 @@ export default function Management() {
     getProducts();
   }, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Container className="mt-5 mx-auto">
-        <div align="right" className="mb-3">
-          <Button variant="success" onClick={() => setShowAddModal(true)}>
-            Add Product
-          </Button>
-        </div>
-        <Table bordered>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((value) => {
-              return (
-                <tr key={value.id}>
-                  <td>
-                    <img src={value.image} alt="" />
-                  </td>
-                  <td>{value.name}</td>
-                  <td>฿{value.price}</td>
-                  <td>
-                    <ButtonGroup>
-                      <Button
-                        variant="warning"
-                        onClick={() => {
-                          setShowEditModal(true);
-                          setEditProduct(value);
-                        }}
-                      >
-                        <BsPencilFill />
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => handleDeleteProduct(value.id)}
-                      >
-                        <BsTrash />
-                      </Button>
-                      <Button
-                        variant="secondary"
-                        onClick={() => {
-                          setShowAddInventory(true);
-                          getInventory(value.id);
-                        }}
-                      >
-                        <BsPlus />
-                      </Button>
-                    </ButtonGroup>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </Container>
-      <ModalEditProduct
-        show={showEditModal}
-        closeModal={setShowEditModal}
-        product={editProduct}
-        setProducts={setProducts}
-      />
-      <ModalAddProduct
-        show={showAddModal}
-        closeModal={setShowAddModal}
-        setProducts={setProducts}
-      />
-      <ModalAddInventory
-        show={showAddInventory}
-        closeModal={setShowAddInventory}
-        inventory={inStock}
-      />
-    </div>
-  );
+  if (username !== "admin") {
+    return <Navigate to="/home" replace />;
+  } else {
+    return (
+      <div>
+        <Navigation />
+        <Container className="mt-5 mx-auto">
+          <div align="right" className="mb-3">
+            <Button variant="success" onClick={() => setShowAddModal(true)}>
+              Add Product
+            </Button>
+          </div>
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((value) => {
+                return (
+                  <tr key={value.id}>
+                    <td>
+                      <img src={value.image} alt="" />
+                    </td>
+                    <td>{value.name}</td>
+                    <td>฿{value.price}</td>
+                    <td>
+                      <ButtonGroup>
+                        <Button
+                          variant="warning"
+                          onClick={() => {
+                            setShowEditModal(true);
+                            setEditProduct(value);
+                          }}
+                        >
+                          <BsPencilFill />
+                        </Button>
+                        <Button
+                          variant="danger"
+                          onClick={() => handleDeleteProduct(value.id)}
+                        >
+                          <BsTrash />
+                        </Button>
+                        <Button
+                          variant="secondary"
+                          onClick={() => {
+                            setShowAddInventory(true);
+                            getInventory(value.id);
+                          }}
+                        >
+                          <BsPlus />
+                        </Button>
+                      </ButtonGroup>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Container>
+        <ModalEditProduct
+          show={showEditModal}
+          closeModal={setShowEditModal}
+          product={editProduct}
+          setProducts={setProducts}
+        />
+        <ModalAddProduct
+          show={showAddModal}
+          closeModal={setShowAddModal}
+          setProducts={setProducts}
+        />
+        <ModalAddInventory
+          show={showAddInventory}
+          closeModal={setShowAddInventory}
+          inventory={inStock}
+        />
+      </div>
+    );
+  }
 }
